05c6ef481aab98cffd132d7b316d130b67daf720
Medo101 <107288702+Medo101@users.noreply.github.com>
2024-12-19 01:57:16 +0200

    mesh 3arffffffff

--- src/main/java/WatchIt/Controllers/Account/Client/Pages/InnerPageComponentControllers/InnerPageComponentController.java
+++ src/main/java/WatchIt/Controllers/Account/Client/Pages/InnerPageComponentControllers/InnerPageComponentController.java
@@ -2,11 +2,17 @@ package WatchIt.Controllers.Account.Client.Pages.InnerPageComponentControllers;
 
 import WatchIt.Views.ClientView;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.layout.VBox;
+import src.Cast.CastMember;
 import src.ContentControl.Content;
+import src.DataBase.DataBase;
 import src.DataBase.DataObject;
+import src.DataBase.DataObjectController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InnerPageComponentController {
     Content content;
@@ -20,6 +26,9 @@ public class InnerPageComponentController {
             vbox.getChildren().add(ClientView.HeadComponent(content).load());
             vbox.getChildren().add(ClientView.DescriptionComponent(content.story).load());
             vbox.getChildren().add(ClientView.MediaComponent(content.getVideo()).load());
+            List<Node> list;
+            list=DataObjectController.MakeNodeList(DataBase.getInstance().castMemberData.getDataByString(content.cast,2));
+           vbox.getChildren().add(ClientView.GridMake(list).load());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
